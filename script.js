f7521d07d313475a416b30314571b59d3f9e8b61
Mcdroe <43692053+Mc0717@users.noreply.github.com>
2026-02-19 00:28:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-// Fade-in effect on scroll
-const faders = document.querySelectorAll('.fade-in');
+// Hamburger menu
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
+
+// Fade-in on scroll
+const faders = document.querySelectorAll('.fade-in-section');
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        if (entry.isIntersecting) {
+        if(entry.isIntersecting){
             entry.target.classList.add('show');
         }
     });
-}, { threshold: 0.3 });
+}, { threshold:0.3 });
 
 faders.forEach(fade => observer.observe(fade));
